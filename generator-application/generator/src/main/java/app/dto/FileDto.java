e03987ef81440ea9d77b9fc11b8df1f2ac23b159
Miguel Osorio <miguelangel.osorio@mercadolibre.com.co>
2024-11-08 20:01:02 -0500

    fix dto2

--- generator-application/generator/src/main/java/app/dto/FileDto.java
+++ generator-application/generator/src/main/java/app/dto/FileDto.java
@@ -1,8 +1,11 @@
 package app.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.io.Serializable;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class FileDto implements Serializable {
     private String name;
     private String signed;
@@ -13,4 +16,19 @@ public class FileDto implements Serializable {
         this.signed = (fileSignature != null && !fileSignature.isEmpty()) ? "firmado" : "no firmado";
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSigned() {
+        return signed;
+    }
+
+    public void setSigned(String signed) {
+        this.signed = signed;
+    }
 }
